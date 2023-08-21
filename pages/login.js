c07c0853f9cfccd603c510f59dd0f46b841cccfe
NEKiarie <s49188705@gmail.com>
2023-08-21 11:09:23 +0300

    :lipstick:Added styling to Login button

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import Layout from "../layout/layout"
 import Link from "next/link"
+import styles from "../styles/Form.module.css"
 
 export default function Login() {
     return (
@@ -11,37 +12,44 @@ export default function Login() {
             </Head>
 
             <section className="w-3/4 mx-auto flex flex-col gap-10">
+
                 <div className="title">
                     <h1 className="text-gray-800 text-4xl font-bold py-4">Explore</h1>
                     <p className="w-3/4 mx-auto text-gray-400">Explore more by connecting with us!</p>
                 </div>
 
                 {/**form */}
-                <form classname="flex flex-col gap-5">
-                    <div className="input-group">
-                        <input type="email" name="email" placeholder="Email" />
+
+                {/* <form classname="flex flex-col gap-5"> */}
+                    <div className={styles.input_group}>
+                        <input className={styles.input_text}
+                            type="email"
+                            name="email"
+                            placeholder="Email" />
 
                     </div>
-                    <div className="input-group">
-                        <input type="password" name="password" placeholder="Password" />
+                {/* </form> */}
+                <div className={styles.input_group}>
+                    <input className={styles.input_text}
+                        type="password"
+                        name="password"
+                        placeholder="Password" />
 
-                    </div>
-                    {/**Login Buttons */}
-                    <div className="input-button">
-                        <button type="submit">Login</button>
-                    </div>
-                    <div className="input-button">
-                        <button type="submit">Sign In with Google</button>
+                </div>
+                {/**Login Buttons */}
+                <div className="input-button" >
+                    <button type="submit" className={styles.button}>Login</button>
+                </div>
+                <div className={styles.button_custom}>
+                    <button type="button">Sign In with Google</button>
 
-                    </div>
-                    <div className="input-button">
-                        <button type="submit">Sign In with Github</button>
-                    </div>
-
-                </form>
+                </div>
+                <div className={styles.button_custom}>
+                    <button type="button">Sign In with Github</button>
+                </div>
                 {/**Bottom */}
                 <p className="text-centertext-gray-400">Don't have an Account yet?
-                 <Link href={'/register'}><a className="text-blue-700">Sign Up</a></Link></p>
+                    <Link href={'/register'}><a className="text-blue-700"> Sign Up</a></Link></p>
 
             </section>
 
